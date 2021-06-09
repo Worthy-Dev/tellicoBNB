fca474e85fd54847e146127a1520559e6f82e451
katieolson84 <katieolson84@gmail.com>
2021-06-08 21:49:28 -0600

    added logo images

--- tellico/src/components/Nav.js
+++ tellico/src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components';
 
 const Nav = () => {
     return (
@@ -7,6 +8,7 @@ const Nav = () => {
         </div>
     )
 }
+
 
 export default Nav
 
