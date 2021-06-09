763cde25c86bb8d3a6600808ced6babd5a973567
mworth <mikeworthen55@gmail.com>
2021-06-08 21:20:30 -0600

    Added the structure of the app using react.

---
+++ tellico/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+// import Pages
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import Gallery from './pages/Gallery';
+import Home from './pages/Home';
+import Services from './pages/Services';
+// Router
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+        <Nav />
+      
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/services">
+          <Services />
+        </Route>
+
+        <Route exact path="/about">
+          <AboutUs />
+        </Route>
+
+        <Route exact path="/gallery">
+          <Gallery />
+        </Route>
+
+        <Route exact path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
